70738f2ffb2f9d742b16bc7de8b4c46a330215b4
Mafinar Khan <mafinar@gmail.com>
2020-01-10 23:28:10 -0500

    Refactor

--- ninetynine/list.ml
+++ ninetynine/list.ml
@@ -3,36 +3,36 @@ exception NOT_ENOUGH_ELEMENT
 exception INDEX_OUT_OF_BOUND
 
 (* [P-1] Find the last element of a list. *)
-let rec last' xs = match xs with
+let rec last = function
   | [] -> raise EMPTY_LIST
   | [x] -> x
-  | _ :: xs -> find_last xs
+  | _ :: xs -> last xs
 
 (* [P-2] Find the last but one element of a list. *)
-let rec butlast' xs = match xs with
+let rec butlast = function
   | [] -> raise EMPTY_LIST
   | [_] -> raise NOT_ENOUGH_ELEMENT
-  | x :: _ :: [] -> x
+  | [x; _] -> x
   | _ :: x :: [_] -> x
-  | _ :: xs -> find_butlast xs
+  | _ :: xs -> butlast xs
 
 (* [P-3] Find the K'th element of a list. *)
-let rec at' xs k = match xs, k with
+let rec at xs k = match xs, k with
   | [], _ -> raise INDEX_OUT_OF_BOUND
   | x::_, k when k == 0 -> x
-  | _::xs', k -> at' xs' (k - 1) 
+  | _::xs', k -> at xs' (k - 1) 
 
 (* [P-4] Find the number of elements of a list. *)
-let rec length' xs = match xs with
+let rec len = function
   | [] -> 0
-  | _::xs' -> 1 + (length' xs') 
+  | _::xs -> 1 + (len xs) 
 
 (* [P-5] Reverse a list. *)
-let reverse' xs = 
-  let rec do_reverse' acc xxs = match acc, xxs with
-    | acc, [] -> acc
-    | acc, x::xxs' -> do_reverse' (x::acc) xxs' in
-  do_reverse' [] xs
+let rev xs = 
+  let rec aux acc = function
+    | [] -> acc
+    | x::xs -> aux (x::acc) xs in
+  aux [] xs
 
 (* [P-6] Find out whether a list is a palindrome. *)
-let palindrome' xs = xs = (reverse' xs)
+let is_palin xs = xs = (rev xs)
